b6ea370544790b07fb454ff4728e8cdbc2617b9a
javiergimenez <93931108+javigimu@users.noreply.github.com>
2022-12-08 15:04:25 +0100

    Documentación con SandCastle

--- VentasDAM/Gimenez_Javier_VENTASDAM/DatosEEF/EmployeeADO.cs
+++ VentasDAM/Gimenez_Javier_VENTASDAM/DatosEEF/EmployeeADO.cs
@@ -10,6 +10,9 @@ using System.Threading.Tasks;
 namespace DatosEEF
 {
     /// <summary>
+    /// Clase EmployeeADO.
+    /// Clase que gestiona las operaciones necesarias en la base de datos para los empleados.
+    /// <see cref="Employee"/>
     /// <autor>Javier Giménez</autor>
     /// </summary>
     public class EmployeeADO : IDisposable
@@ -17,12 +20,26 @@ namespace DatosEEF
         // Flag: Se ha llamado a Dispose?
         bool disposed;
 
+        /// <summary>
+        /// Constructor por defecto
+        /// </summary>
         public EmployeeADO()
         {
             disposed = false;
         }
 
         // Métodos CRUD (Listar, Insertar, Actualizar, Borrar)
+        /// <summary>
+        /// Obtiene de la base de datos el listado completo de empleados
+        /// </summary>
+        /// <returns>Lista de empleados</returns>
+        /// <example>Muestra como se obtiene una lista de la tabla Employees desde el contexto NorthwindContext
+        /// <code>var data = context.Employees.ToList()</code>
+        /// </example>
+        /// <remarks>
+        /// Obtención de una lista de <see cref="Employee"/> independiente del tipo de base de datos.
+        /// Obtiene la lista de empleados ordenados por <see cref="Employee.FirstName"/> 
+        /// y <see cref="Employee.LastName"/></remarks>
         static public IList<Employee> Listar()
         {
             using (var context = new NorthwindContext())
@@ -36,7 +53,22 @@ namespace DatosEEF
             }
         }
 
-        // Listar datos de un empleado
+        /// <summary>
+        /// Obtiene los datos de un empleado
+        /// </summary>
+        /// <param name="ID">Identificador del empleado: EmployeeId</param>
+        /// <returns>Un objeto empleado</returns>
+        /// <example>Muestra como se obtiene un <see cref="Employee"/> con <see cref="Employee.EmployeeId"/> 5
+        /// <code>
+        /// var query = from st in context.Employees
+        ///     where st.EmployeeId == 5
+        ///     select st;
+        /// </code>
+        /// </example>
+        /// <remarks>
+        /// Obtención de un <see cref="Employee"/> independiente del tipo de base de datos.
+        /// Si no se encuentra el Id de empleado devuelve null, sino obtiene todos los datos del objeto.
+        /// <see cref="Employee"/> de la base de datos</remarks>
         static public Employee? Listar(int ID)
         {
             using (var context = new NorthwindContext())
@@ -50,7 +82,23 @@ namespace DatosEEF
             }
         }
 
-        // Lista todos los OrderId de un empleado
+        /// <summary>
+        /// Obtiene todos los Id de pedido de un empleado
+        /// </summary>
+        /// <param name="ID">Identificador del empleado: EmployeeId</param>
+        /// <returns>Lista de Id de pedidos</returns>
+        /// <see cref="Order"/>
+        /// <example>Muestra de cómo obtener el listado de todos los <see cref="Order.OrderId"/> del empleado <see cref="Employee"/> 5
+        /// <code>
+        /// var query = from st in context.Orders
+        ///     where st.EmployeeId == 5
+        ///     select st.OrderId
+        /// </code>
+        /// </example>
+        /// <remarks>
+        /// Obtención de Lista de <see cref="Order.OrderId"/> independiente del tipo de base de datos.
+        /// El método debe recibir un <see cref="Employee.EmployeeId"/> existente o devolverá una lista vacía.
+        /// </remarks>
         static public List<int> ListarPedidosId(int ID)
         {
             using (var context = new NorthwindContext())
@@ -64,7 +112,23 @@ namespace DatosEEF
             }
         }
 
-        // Lista todos los pedidos de un empleado
+        /// <summary>
+        /// Obtiene una colección con todos los pedidos completos de un empleado
+        /// </summary>
+        /// <param name="ID">Identificador del empleado: EmployeeId</param>
+        /// <returns>Colección de pedidos de un empleado</returns>
+        /// <see cref="Order"/>
+        /// /// <example>Muestra de cómo obtener el listado de todos los <see cref="Order"/> del <see cref="Employee.EmployeeId"/> 5
+        /// <code>
+        /// var query = from st in context.Orders
+        ///     where st.EmployeeId == 5
+        ///     select st
+        /// </code>
+        /// </example>
+        /// <remarks>
+        /// Obtención de pedidos independiente del tipo de base de datos.
+        /// El método debe recibir un <see cref="Employee.EmployeeId"/> existente o devolverá null.
+        /// </remarks>
         static public ICollection<Order>? ListarPedidos(int ID)
         {
             using (var context = new NorthwindContext())
@@ -78,6 +142,19 @@ namespace DatosEEF
             }
         }
 
+        /// <summary>
+        /// Inserta un nuevo empleado en la base de datos
+        /// </summary>
+        /// <param name="dato">Nuevo empleado</param>
+        /// <example>Muestra de inserción de un <see cref="Employee"/> empleado en la base de datos
+        /// <code>
+        /// context.Entry(empleado).State = EntityState.Added;
+        /// context.SaveChanges();
+        /// </code>
+        /// </example>
+        /// <remarks>
+        /// Inserción de un <see cref="Employee"/> en la base de datos independiente del tipo de base de datos
+        /// </remarks>
         public void Insertar(Employee dato)
         {
             using (var context = new NorthwindContext())
@@ -87,6 +164,19 @@ namespace DatosEEF
             }
         }
 
+        /// <summary>
+        /// Actualiza un empleado existente en la base de datos
+        /// </summary>
+        /// <param name="modificado">Empleado a modificar</param>
+        /// <example>Muestra de actualización de un <see cref="Employee"/> empleado
+        /// <code>
+        /// context.Entry(empleado).State = EntityState.Modified;
+        /// context.SaveChanges();
+        /// </code>
+        /// </example>
+        /// <remarks>
+        /// Actualización de un <see cref="Employee"/> independiente del tipo de la base de datos.
+        /// </remarks>
         public void Actualizar(Employee modificado)
         {
             using (var context = new NorthwindContext())
@@ -96,6 +186,10 @@ namespace DatosEEF
             }
         }
 
+        /// <summary>
+        /// Borra un empleado de la base de datos
+        /// </summary>
+        /// <param name="dato">Empleado a borrar</param>
         public void Borrar(Employee dato)
         {
             using (var context = new NorthwindContext())
@@ -105,15 +199,19 @@ namespace DatosEEF
             }
         }
 
-
-        // Public implementation of Dispose pattern callable by consumers.
+        /// <summary>
+        /// Implementación de Dispose para liberar recursos
+        /// </summary>
         public void Dispose()
         {
             Dispose(disposing: true);
             GC.SuppressFinalize(this);
         }
 
-        // Protected implementation of Dispose pattern.
+        /// <summary>
+        /// Implementación protegida del método Dispose
+        /// </summary>
+        /// <param name="disposing">Booleano que indica se ha llamado a Dispose</param>
         protected virtual void Dispose(bool disposing)
         {
             if (disposed)
--- VentasDAM/Gimenez_Javier_VENTASDAM/Entidades/Employee.cs
+++ VentasDAM/Gimenez_Javier_VENTASDAM/Entidades/Employee.cs
@@ -9,64 +9,151 @@ using Microsoft.EntityFrameworkCore;
 namespace Modelos
 {
     /// <summary>
-    /// <autor>Javier Giménez</autor>
+    /// Clase Employee.
+    /// Version 1.0
+    /// <autor>Javier Giménez</autor> 
     /// </summary>
-
     [Index("LastName", Name = "LastName")]
-    [Index("PostalCode", Name = "PostalCode")]
+    [Index("PostalCode", Name = "PostalCode")]    
     public partial class Employee : IComparable<Employee>
     {       
+        /// <summary>
+        /// Clave primaria del empleado.
+        /// </summary>
         [Key]
         [Column("EmployeeID")]
         public int EmployeeId { get; set; }
+        /// <summary>
+        /// Apellido del empleado con longitud máxima de 20 caracteres
+        /// </summary>
         [StringLength(20)]
         public string LastName { get; set; } = null!;
+        /// <summary>
+        /// Nombre del empleado con longitud máxima de 10 caracteres
+        /// </summary>
         [StringLength(10)]
         public string FirstName { get; set; } = null!;
+        /// <summary>
+        /// Cargo del empleado con longitud máxima de 30 caracteres
+        /// </summary>
         [StringLength(30)]
         public string? Title { get; set; }
+        /// <summary>
+        /// Título de cortesía con longitud máxima de 25 caracteres
+        /// </summary>
         [StringLength(25)]
         public string? TitleOfCourtesy { get; set; }
+        /// <summary>
+        /// Fecha de nacimiento del empleado
+        /// </summary>
         [Column(TypeName = "datetime")]
         public DateTime? BirthDate { get; set; }
+        /// <summary>
+        /// Fecha de contratación del empleado
+        /// </summary>
         [Column(TypeName = "datetime")]
         public DateTime? HireDate { get; set; }
+        /// <summary>
+        /// Dirección de residencia del empleado con longitud máxima de 60 caracteres
+        /// </summary>
         [StringLength(60)]
         public string? Address { get; set; }
+        /// <summary>
+        /// Ciudad de residencia del empleado con longitud máxima de 15 caracteres
+        /// </summary>
         [StringLength(15)]
         public string? City { get; set; }
+        /// <summary>
+        /// Provincia de residencia del empleado con longitud máxima de 15 caracteres
+        /// </summary>
         [StringLength(15)]
         public string? Region { get; set; }
+        /// <summary>
+        /// Código postal de residencia del empleado con longitud máxima de 10 caracteres
+        /// </summary>
         [StringLength(10)]
         public string? PostalCode { get; set; }
+        /// <summary>
+        /// País de residencia del empleado con longitud máxima de 15 caracteres
+        /// </summary>
         [StringLength(15)]
         public string? Country { get; set; }
+        /// <summary>
+        /// Teléfono personal del empleado con longitud máxima de 24 caracteres
+        /// </summary>
         [StringLength(24)]
         public string? HomePhone { get; set; }
+        /// <summary>
+        /// Extensión para el teléfono del empleado con longitud máxima de 4 caracteres
+        /// </summary>
         [StringLength(4)]
         public string? Extension { get; set; }
+        /// <summary>
+        /// Foto personal para identificar al empleado
+        /// </summary>
         [Column(TypeName = "image")]
         public byte[]? Photo { get; set; }
+        /// <summary>
+        /// Información adicional del empleado
+        /// </summary>
         public string? Notes { get; set; }
+        /// <summary>
+        /// Identificador del empleado supervisor
+        /// </summary>
         public int? ReportsTo { get; set; }
+        /// <summary>
+        /// Ruta de acceso a la foto del empleado con longitud máxima de 255 caracteres
+        /// </summary>
         [StringLength(255)]
         public string? PhotoPath { get; set; }
 
+        /// <summary>
+        /// Clave ajena a Employees para enlazar los reportes al supervisor
+        /// </summary>
         [ForeignKey("ReportsTo")]
         [InverseProperty("InverseReportsToNavigation")]
         public virtual Employee? ReportsToNavigation { get; set; }
-        [InverseProperty("ReportsToNavigation")]
+        /// <summary>
+        /// Propiedad inversa para gestionar los reportes al supervisor
+        /// </summary>
+        [InverseProperty("ReportsToNavigation")]        
         public virtual ICollection<Employee>? InverseReportsToNavigation { get; set; }
+        /// <summary>
+        /// Colección con los pedidos realizados por el empleado
+        /// </summary>
         [InverseProperty("Employee")]
         public virtual ICollection<Order>? Orders { get; set; }
 
+        /// <summary>
+        /// Constructor por defecto
+        /// </summary>
         public Employee()
         {
             InverseReportsToNavigation = new HashSet<Employee>();
             Orders = new HashSet<Order>();
         }
-        
-        // Constructor con todos los parámetros
+
+        /// <summary>
+        /// Constructor con todos los parámetros
+        /// </summary>
+        /// <param name="employeeId">Id de empleado</param>
+        /// <param name="lastName">Apellido del empleado</param>
+        /// <param name="firstName">Nombre del empleado</param>
+        /// <param name="title">Cargo del empleado</param>
+        /// <param name="titleOfCourtesy">Título de cortesía (Mr., Ms., Dr. Mrs.)</param>
+        /// <param name="birthDate">Año de nacimiento del empleado</param>
+        /// <param name="hireDate">Año de contratación del empleado</param>
+        /// <param name="address">Dirección del empleado</param>
+        /// <param name="city">Ciudad de residencia del empleado</param>
+        /// <param name="region">Provincia de residencia del empleado</param>
+        /// <param name="postalCode">Código postal de residencia del empleado</param>
+        /// <param name="country">País de residencia del empleado</param>
+        /// <param name="homePhone">Télefono personal del empleado</param>
+        /// <param name="extension">Extensión del teléfono del empleado</param>
+        /// <param name="photo">Foto identificación del empleado</param>
+        /// <param name="notes">Información adicional del empleado</param>
+        /// <param name="photoPath">Ruta de acceso a la foto del empleado</param>
+        /// <param name="reportsTo">Empleado supervisor del empleado</param>
         public Employee(int employeeId, string lastName, string firstName, 
             string? title, string? titleOfCourtesy, DateTime? birthDate, 
             DateTime? hireDate, string? address, string? city, string? region, 
@@ -93,15 +180,23 @@ namespace Modelos
             Notes = notes;
             PhotoPath = photoPath;
             ReportsTo = reportsTo;
-        }        
+        }
 
-        // Constructor con los parámetros obligatorios
+        /// <summary>
+        /// Constructor con los parámetros obligatorios
+        /// </summary>
+        /// <param name="employeeId">Código de identificación del empleado</param>
+        /// <param name="lastName">Apellido del empleado</param>
+        /// <param name="firstName">Nombre del empleado</param>
         public Employee(int employeeId, string lastName, string firstName)
             :this(employeeId, lastName, firstName, null, null, null, null, null, null, null,
                  null, null, null, null, null, null, null, -1)
         { }
 
-        // Constructor de copia
+        /// <summary>
+        /// Constructor de copia
+        /// </summary>
+        /// <param name="e">Empleado del que se realiza la copia</param>
         public Employee(Employee e)
         {
             EmployeeId = e.EmployeeId;
@@ -144,7 +239,14 @@ namespace Modelos
             Notes = e.Notes;           
         }
 
-        // Ordenado por Firstname y después por Lastname
+        /// <summary>
+        /// Método de comparación.
+        /// La interfaz IComparable exige la implementación de un método CompareTo.
+        /// Se realiza la ordenación por Firstname y después por Lastname
+        /// </summary>
+        /// <param name="otro">Segundo empleado para realizar la comparación</param>
+        /// <returns>-1 si el empleado actual es menor que el recibido por parámetro, 
+        /// 0 si los dos empleados son iguales o 1 si el empleado actual es mayor que el recibido por parámetro</returns>
         public int CompareTo(Employee? otro)
         {            
             int resultado = 0;
@@ -163,6 +265,10 @@ namespace Modelos
             return resultado;
         }
 
+        /// <summary>
+        /// Sobrecarga del método ToString.
+        /// </summary>
+        /// <returns>Una cadena de texto con todos los datos del empleado separados por #</returns>
         public override string ToString()
         {
             return EmployeeId + "#" + LastName + "#" + FirstName
@@ -172,6 +278,9 @@ namespace Modelos
                 + "#" + Extension + "#" + Photo + "#" + Notes;
         }
 
+        /// <summary>
+        /// Destructor
+        /// </summary>
         ~Employee()
         {
             if (Photo != null)
